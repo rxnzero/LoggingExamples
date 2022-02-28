dce6c30cf3224724ca00174fdc3cb514555c7ab0
dhlee <dhlee@eactive.co.kr>
2022-02-28 10:55:45 +0900

    use MDC

--- src/com/dhlee/logging/log4j2/RoutingAppenderTest.java
+++ src/com/dhlee/logging/log4j2/RoutingAppenderTest.java
@@ -1,13 +1,13 @@
 package com.dhlee.logging.log4j2;
 
 //import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.ThreadContext;
+//import org.apache.logging.log4j.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 
 public class RoutingAppenderTest {
-
 //	Logger log = LogManager.getLogger(this.getClass().getCanonicalName());
 	static Logger log = LoggerFactory.getLogger(RoutingAppenderTest.class);
 	public RoutingAppenderTest() {
@@ -21,13 +21,19 @@ public class RoutingAppenderTest {
 	}
 	
 	public void log(String groupId, String threadId) {
-		ThreadContext.put("groupId", groupId);
-	    ThreadContext.put("threadId", threadId);
+//		ThreadContext.put("groupId", groupId);
+//	    ThreadContext.put("threadId", threadId);
+	    MDC.put("groupId", groupId);
+	    MDC.put("threadId", threadId);
+	    
 	    log.info("Parsing file " + threadId + " from " + groupId);
 	    log.info("Uploading data from " + threadId + " from " + groupId);
 	    log.info("Generating invoices from " + threadId + " from " + groupId);
 	    log.info("Processing completed **** " + threadId + " from " + groupId);
-	    ThreadContext.remove("groupId");
-	    ThreadContext.remove("threadId");
+//	    ThreadContext.remove("groupId");
+//	    ThreadContext.remove("threadId");
+	    
+	    MDC.remove("groupId");
+	    MDC.remove("threadId");
 	}
 }
